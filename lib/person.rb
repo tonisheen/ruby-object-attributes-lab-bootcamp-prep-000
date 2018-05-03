80309941b99e0ff5f01c4f891afd66f35389f4ea
Newton Migosi <newtonmigosi@gmail.com>
2018-05-03 10:02:46 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,22 +1,22 @@
 class Person
-  def initialize(name, job)
-    @person_name = name
-    @person_job = job
-  end
+  #def initialize(name, job)
+    #@person_name = name
+    #@person_job = job
+  #end
   
   def name
-    @person_name
+    @name
   end
   
   def name=(new_name)
-    @person_name = new_name
+    @name = new_name
   end
   
   def job
-    @person_job
+    @job
   end
   
   def job=(new_job)
-    @person_job = new_job
+    @job = new_job
   end
 end
